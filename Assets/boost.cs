e1458d835d03dd2a2582dc596cacd0177bf65882
Leanne Sall <lsall1@my.bcit.ca>
2020-11-05 08:23:14 -0800

    Added thrusters

--- Assets/boost.cs
+++ Assets/boost.cs
@@ -1,19 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class boost : MonoBehaviour
 {
+
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
     }
-   
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotating right");
+        }
+
+
+
+    }
+
 }
