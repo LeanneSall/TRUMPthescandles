5acfbb42d5ad076ba055f2d477ea073829c3bd26
Leanne Sall <lsall1@my.bcit.ca>
2020-11-05 14:02:43 -0800

    added some obstacles

--- Assets/boost.cs
+++ Assets/boost.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 
 public class boost : MonoBehaviour
 {
-
+    [SerializeField] float rcsRotate = 100f;
+    [SerializeField] float rcsThrust = 1000f;
     Rigidbody rigidbody;
     AudioSource china;
 
@@ -21,35 +22,61 @@ public class boost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("dead"); 
+                break;
+        }
+    }
+
+    void Rotate()
+    {
+        rigidbody.freezeRotation = true;
+       
+        float rotationThisFrame = rcsRotate * Time.deltaTime;
+        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false;
+
+    }
+
+    private void Thrust()
+    {
+        
+        float forwardThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
-        {            
-            rigidbody.AddRelativeForce(Vector3.up);
+        {
+            rigidbody.AddRelativeForce(Vector3.up * forwardThisFrame);
             if (!china.isPlaying)
             {
                 china.Play();
             }
 
-        } else
+        }
+        else
         {
             china.Stop();
         }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-        }
-
-
-
     }
-
 }
